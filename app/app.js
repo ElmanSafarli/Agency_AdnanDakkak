494910e350cdf9007524649da693c6025037afbf
Elman Seferli <itiselman@gmail.com>
2025-03-22 10:19:38 +0400

    update

--- app/app.js
+++ app/app.js
@@ -16,3 +16,59 @@ $(document).ready(function () {
     arrow.toggleClass("rotate");
   });
 });
+
+// gsap.registerPlugin(Draggable);
+// // Get the parent container dimensions
+
+// let parent = document.querySelector(".offer_services_content");
+// let items = document.querySelectorAll(".offer_services-drag-item");
+
+// // Create draggable instances
+// let draggables = Draggable.create(items, {
+//   type: "x,y",
+//   bounds: parent, // Keeps items inside parent
+//   edgeResistance: 1, // No edge resistance
+//   inertia: false, // Prevents excessive movement
+//   onDrag: function () {
+//     checkCollisions(this);
+//   },
+// });
+
+// function checkCollisions(draggedItem) {
+//   let draggedRect = draggedItem.target.getBoundingClientRect();
+
+//   items.forEach((otherItem) => {
+//     if (otherItem !== draggedItem.target) {
+//       let otherRect = otherItem.getBoundingClientRect();
+
+//       if (isColliding(draggedRect, otherRect)) {
+//         resolveCollision(draggedItem, otherItem);
+//       }
+//     }
+//   });
+// }
+
+// function isColliding(rect1, rect2) {
+//   return (
+//     rect1.left < rect2.right &&
+//     rect1.right > rect2.left &&
+//     rect1.top < rect2.bottom &&
+//     rect1.bottom > rect2.top
+//   );
+// }
+
+// function resolveCollision(draggedItem, otherItem) {
+//   let dx = draggedItem.x - otherItem.offsetLeft;
+//   let dy = draggedItem.y - otherItem.offsetTop;
+//   let distance = Math.sqrt(dx * dx + dy * dy) || 1;
+
+//   let moveX = (dx / distance) * 5; // Soft repulsion (adjust force here)
+//   let moveY = (dy / distance) * 5;
+
+//   gsap.to(otherItem, {
+//     x: `+=${moveX}`,
+//     y: `+=${moveY}`,
+//     duration: 0.3,
+//     ease: "power2.out",
+//   });
+// }
---
+++ app/detail.js
@@ -0,0 +1,39 @@
+const swiper = new Swiper(".portfolio_slider", {
+  // Optional parameters
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 10,
+
+  // If we need pagination
+  //   pagination: {
+  //     el: ".swiper-pagination-projects",
+  //     clickable: true,
+  //     dynamicBullets: true,
+  //   },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: "#next_btn_projects",
+    prevEl: "#prev_btn_projects",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
